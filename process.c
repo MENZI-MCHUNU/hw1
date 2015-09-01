59b6d7619b615c1000c13963c63d4e9b1faeb8ad
MENZI-MCHUNU <menzi639@gmail.com>
2015-09-02 00:43:04 +0200

    Part 6,7

--- process.c
+++ process.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include <termios.h>
 
+#define WAIT_ANY -1
 /**
  * Executes the process p.
  * If the shell is in interactive mode and the process is a foreground process,
@@ -14,81 +15,67 @@
 void launch_process(process *p)
 {
 
-  /** YOUR CODE HERE */
-    pid_t pid ;
-    pid_t pgid;
-   int infile;
-    int outfile;
-   int errfile;    
-      int foreground = 0;
-    if (shell_is_interactive){
-           pid =getpid();
-           if(pgid == 0)pgid = pid;
-              setpgid(pid,pgid);
-             if(foreground)
-              tcsetpgrp(shell_terminal,pgid);
-              
-              
-              /* Set the handling for job control signals back to the default.  */
-       
+ pid_t pid;
+ pid_t pgid = p->pgid;
+
+  if (shell_is_interactive)
+    {
+      /* Put the process into the process group and give the process group
+         the terminal, if appropriate.
+         This has to be done both by the shell and in the individual
+         child processes because of potential race conditions.  */
+      pid = getpid ();
+      if (pgid == 0) pgid = pid;
+      setpgid (pid, pgid);
+	tcsetpgrp (shell_terminal, pgid);
+      
+      /* Set the handling for process control signals back to the default.  */
       signal (SIGINT, SIG_DFL);
       signal (SIGQUIT, SIG_DFL);
       signal (SIGTSTP, SIG_DFL);
       signal (SIGTTIN, SIG_DFL);
       signal (SIGTTOU, SIG_DFL);
       signal (SIGCHLD, SIG_DFL);
-            
-         }   
-     /* Set the standard input/output channels of the new process.  */
       
-     /* if(infile !=STDIN_FILENO){
-       dup2(infile, STDIN_FILENO);
-       close(infile);
-      }
-       if(outfile !=STDOUT_FILENO){
-        dup2(outfile,STDOUT_FILENO);
-        close(outfile);
-       }
-       
-       if(errfile !=STDERR_FILENO){
-        dup2(errfile,STDERR_FILENO);
-        close(errfile); 
-       }
-       */
+   }
+
+/* Exec the new process.  Make sure we exit.  */
         if((execvp(*p->argv,p->argv))<0){
        	      perror("execvp");
         	exit(1); 
     }
+    
 }
 
-/* Put a process in the foreground. This function assumes  that the shell
+/* Put a process in the foreground. This function assumes that the shell
  * is in interactive mode. If the cont argument is true, send the process
  * group a SIGCONT signal to wake it up.
  */
 void
 put_process_in_foreground (process *p, int cont)
 {
-  /** YOUR CODE HERE */
-  
-     /* Put the process into the foreground.  */
-  tcsetpgrp (shell_terminal, /*j*/p->pgid);
 
-  /* Send the job a continue signal, if necessary.  */
+    pid_t pid;
+ /* Put the process into the foreground.  */
+  tcsetpgrp (shell_terminal, p->pgid);
+
+  /* Send the process a continue signal, if necessary.  */
   if (cont)
     {
-      tcsetattr (shell_terminal, TCSADRAIN, /*&j*/&p->tmodes);
-      if (kill (- /*j*/p->pgid, SIGCONT) < 0)
+
+      tcsetattr (shell_terminal, TCSADRAIN, &p->tmodes);
+      if (kill (- p->pgid, SIGCONT) < 0)
         perror ("kill (SIGCONT)");
     }
 
   /* Wait for it to report.  */
-  //wait_for_process (p);
+    pid = waitpid (WAIT_ANY, &cont, WUNTRACED);
 
   /* Put the shell back in the foreground.  */
   tcsetpgrp (shell_terminal, shell_pgid);
 
-  /* Restore the shell's terminal modes.  */
-  tcgetattr (shell_terminal, /*&j*/&p->tmodes);
+  /* Restore the shell’s terminal modes.  */
+  tcgetattr (shell_terminal, &p->tmodes);
   tcsetattr (shell_terminal, TCSADRAIN, &shell_tmodes);
 }
 
@@ -97,13 +84,10 @@ put_process_in_foreground (process *p, int cont)
 void
 put_process_in_background (process *p, int cont)
 {
-  /** YOUR CODE HERE */
-/* Send the process a continue signal, if necessary.  */
 
-  signal (SIGCHLD, SIG_IGN);
-    if (cont)
+/* Send the process a continue signal, if necessary.  */
+  if (cont)
     if (kill (-p->pgid, SIGCONT) < 0)
       perror ("kill (SIGCONT)");
-   
-  
+
 }
--- shell.c
+++ shell.c
@@ -8,60 +8,102 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdbool.h>
-#include <fcntl.h> 
-#define WAIT_ANY        (-1)    /* any process */
+#include <linux/limits.h>
 #define INPUT_STRING_SIZE 80
+#define DELIMS " \t\r\n"
+#define MAX_LENGTH 1024
+
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "io.h"
 #include "parse.h"
 #include "process.h"
 #include "shell.h"
 
+#define BUFFERSIZE 200
+
+#define STDIN 1
+#define STDOUT 2
+
+#define MAX_BUFFER 1024                        // max line buffer
+#define MAX_ARGS 64                            // max # args
+#define SEPARATORS " \t\n"                     // token sparators
+
+
+extern char **environ;
+
 int cmd_quit(tok_t arg[]) {
-    printf("Bye\n");
-    exit(0);
-    return 1;
+  printf("Bye\n");
+  exit(0);
+  return 1;
 }
 
 int cmd_help(tok_t arg[]);
+int cmd_and(tok_t arg[],char *c);
 int cmd_cd(tok_t arg[]);
-int cmd_amp(tok_t arg[]);
+
 /* Command Lookup table */
 typedef int cmd_fun_t (tok_t args[]); /* cmd functions take token array and return int */
 typedef struct fun_desc {
-    cmd_fun_t *fun;
-    char *cmd;
-    char *doc;
+  cmd_fun_t *fun;
+  char *cmd;
+  char *doc;
+
+ 
 } fun_desc_t;
 
 fun_desc_t cmd_table[] = {
-    {cmd_help, "?", "show this help menu"},
-    {cmd_quit, "quit", "quit the command shell"},
-    {cmd_cd, "cd", "change Directory"},
-    {cmd_amp,"&",""},
+  {cmd_help, "?", "show this help menu"},
+  {cmd_quit, "quit", "quit the command shell"},
+  {cmd_cd, "cd", "change Directory"},
+  {cmd_and, "&", ""},
 };
 
-int cmd_amp(tok_t arg[]){
- tok_t *t;
- process *p;
-  int cont;
+int cmd_help(tok_t arg[]) {
+  int i;
+  for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
+    printf("%s - %s\n",cmd_table[i].cmd, cmd_table[i].doc);
+  }
+  return 1;
+  
+}
 
-
-  if((arg[0]) == NULL){
-     
-     put_process_in_background (p,cont);
-      // signal (SIGCHLD, SIG_IGN);
-      }
-    else if (strcmp(arg[0],"&") == 0){
-        put_process_in_background (p,cont);
-        
-      }
+int lookup(char cmd[]) {
+  int i;
+  for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
+    if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0)) return i;
+  }
+  return -1;
 
 }
-          
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+int cmd_and(tok_t arg[],char *c){
+   
+    process *p;
+    int cont;    
+
+
+   if(arg[0] =="&"){
+     //put_process_in_background (p,cont);
+         } 
+   else if(c!="&"){
+     arg[0]==NULL;
  
+    }
+
+  return 0 ;
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
 int cmd_cd(tok_t arg[]){
-  char cwd[1024];
+     char cwd[1024];
 
      if(arg[0] == NULL){
      	chdir(getenv("HOME"));
@@ -71,73 +113,64 @@ int cmd_cd(tok_t arg[]){
       }
      else
          chdir(arg[0]);
-
-}
-int cmd_help(tok_t arg[]) {
-    int i;
-    for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
-        printf("%s - %s\n",cmd_table[i].cmd, cmd_table[i].doc);
-    }
-    return 1;
 }
 
-int lookup(char cmd[]) {
-    int i;
-    for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
-        if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0)) return i;
-    }
-    return -1;
-}
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
 
 void init_shell()
 {
-    /* Check if we are running interactively */
-    shell_terminal = STDIN_FILENO;
 
-    /** Note that we cannot take control of the terminal if the shell
+    char* cwd;
+    char buff[PATH_MAX + 1];
+   
+  /* Check if we are running interactively */
+  shell_terminal = STDIN_FILENO;
+
+  /** Note that we cannot take control of the terminal if the shell
       is not interactive */
-    shell_is_interactive = isatty(shell_terminal);
+  shell_is_interactive = isatty(shell_terminal);
 
-    if(shell_is_interactive){
+  if(shell_is_interactive){
 
-        /* force into foreground */
-        while(tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp()))
-            kill( - shell_pgid, SIGTTIN);
+    /* force into foreground */
+    while(tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp()))
+      kill( - shell_pgid, SIGTTIN);
 
-
-
-    /* Set the handling for job control signals back to the default.  */
-      
-     signal (SIGINT, SIG_IGN);
-     signal (SIGSTOP, SIG_IGN);
-              
-     signal (SIGQUIT, SIG_IGN);
-     signal (SIGTSTP, SIG_IGN);
-     signal (SIGTTIN, SIG_IGN);
-     signal (SIGTTOU, SIG_IGN);
-   //signal (SIGCHLD, SIG_IGN);
-      
-        shell_pgid = getpid();
-        /* Put shell in its own process group */
-        if(setpgid(shell_pgid, shell_pgid) < 0){
-            perror("Couldn't put the shell in its own process group");
-            exit(1);
-        }
-
-        /* Take control of the terminal */
-        tcsetpgrp(shell_terminal, shell_pgid);
-        tcgetattr(shell_terminal, &shell_tmodes);
+    shell_pgid = getpid();
+    /* Put shell in its own process group */
+    if(setpgid(shell_pgid, shell_pgid) < 0){
+      perror("Couldn't put the shell in its own process group");
+      exit(1);
     }
-    /** YOUR CODE HERE */
+
+    /* Take control of the terminal */
+    tcsetpgrp(shell_terminal, shell_pgid);
+    tcgetattr(shell_terminal, &shell_tmodes);
+  }
+  /** YOUR CODE HERE */
+      signal (SIGINT, SIG_IGN);
+      signal (SIGQUIT, SIG_IGN);
+      signal (SIGTSTP, SIG_IGN);
+      signal (SIGTTIN, SIG_IGN);
+      signal (SIGTTOU, SIG_IGN);
+      signal (SIGCHLD, SIG_IGN);
+      
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
 /**
  * Add a process to our process list
  */
+ 
 void add_process(process* p)
 {
-
-
-process* curr = first_process;	
+  process* curr = first_process;	
 	if(first_process) {
 		while(curr->next) {
 			curr=curr->next;
@@ -147,52 +180,65 @@ process* curr = first_process;
 	} else {
 		first_process = p;
 	}
-
+	
 }
+
+
+/////////////////////////////////////////////////////////////////////////////
+
 
 /**
  * Creates a process given the inputString from stdin
  */
-
 process* create_process(char* inputString)
 {
-      /** YOUR CODE HERE */
-      process* p;
-      p = malloc(sizeof(process));
+      process* p;// Declaring  a new process
+      p = malloc(sizeof(process));//allocating memory for the process
       tok_t *t;
-      t = getToks(inputString);  
-      p->argv = t;
+      t = getToks(inputString);  //getting the tokens from standard input
+      p->argv = t;      
 	int i, n =0; 
       for(i =0; i < MAXTOKS && t[i]; i++){
-        n++;
+        n++;//the number of tokens
+
       }
     p->argc = n;
 
+    p->pgid = getpgrp();
+
+	printf("%d", p->pgid);
+
     return p;
+
 }
 
 
+///////////////////////////////////////////////////////////////////////////////////////////
 
-char * concat(char * s1, char * s2){
-   char * result;
-   result = malloc(strlen(s1)+strlen(s2)+1);
-   strcpy(result,s1);
-   strcat(result,s2);
-return result;
+
+char* concat(char *s1, char *s2)
+{
+    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
+    
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
 }
 
-void path(tok_t * t){
+///////////////////////////////////////////////////////////////////////////////////////////
 
-      char *val;
+
+void path(tok_t *t){
+ char * val;  
+
       val = getenv("PATH");
-      tok_t *paths   =getToks(val);
-     
+
+     tok_t *paths   =getToks(val);
        int i;
 
       for(i =0 ; i < MAXTOKS && paths[i];i++ ){
-      
                       char *path =concat(paths[i], "/");
-                       path =concat(path,t[0]); 
+                       path  =concat(path,t[0]); 
                       if(access(path,F_OK)!=-1){
                             execve(path,t,NULL); 
                          }        
@@ -201,10 +247,11 @@ void path(tok_t * t){
 	      execv(*t,t);
                perror(*t);
                exit(0);
-   
 }
 
-void redirect(tok_t *input,char * filename,char * c){ 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void Redirecting_In_Out(tok_t *input,char * filename,char * c){ 
 int newfd;
   
  if(c == ">"){
@@ -225,86 +272,90 @@ int newfd;
  close(newfd);
  }
  
- path(input);
+// path(input);
  
 }
 
-int shell (int argc, char *argv[]) {
-    char *s = malloc(INPUT_STRING_SIZE+1);			/* user input string */
-    tok_t *t;			/* tokens parsed from input */
-    int lineNum = 0;
-    int fundex = -1;
-    pid_t pid = getpid();		/* get current processes PID */
-    pid_t ppid = getppid();	/* get parents PID */
-    pid_t cpid, tcpid, cpgid;
-      int status;
-     char cwd[1024];
-        process * p;
-      pid_t pgid;
-    printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
-    
-    lineNum=0;
-    fprintf(stdout, "%d%s: ", lineNum,getcwd(cwd,sizeof(cwd)));
-      init_shell();
-    
-    while ((s = freadln(stdin))){
+/////////////////////////////////////////////////////////////////////////////////////////
 
+int shell (int argc, char *argv[]) {
+
+  char *s = malloc(INPUT_STRING_SIZE+1);			/* user input string */
+  tok_t *t;			/* tokens parsed from input */
+  int lineNum = 0;
+  int fundex = -1;
+  pid_t pid = getpid();		/* get current processes PID */
+  pid_t ppid = getppid();	/* get parents PID */
+  pid_t cpid, tcpid, cpgid;
+  char cwd[1024];
+   int status;
+                                                                        
+ 
+   printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
+  
+    lineNum=0;
+    fprintf(stdout, "%d%s:",lineNum,getcwd(cwd,sizeof(cwd)));
+    init_shell();
+
+  while ((s = freadln(stdin))){
+  
         char * temp;
         temp = concat(s,"");
-
-        t = getToks(s); /* break the line into tokens */
-        fundex = lookup(t[0]); /* Is first token a shell literal */
-        if(fundex >= 0) cmd_table[fundex].fun(&t[1]);
-        else {
-           if ((pid = fork()) < 0) {     /* fork a child process           */
-             printf("*** ERROR: forking child process failed\n");
-             exit(1);
-            }
-            else if (pid == 0) {    /* for the child process:         */
-                   process * new =create_process(temp);
-                   add_process(new);
-                   launch_process(new);
-     	       //char *temp = strstr(s,">");
-		//int i=0;
-		//char * a=">", * b = "<";
-              /*  for(i=0;i<MAXTOKS && t[i];i++){
- 
-	        	 if (strcmp(t[i],a)==0){
-
-	 	        t[i]=NULL;
-		       redirect(t,t[i+1],a);	
-                  }
-                       if(strcmp(t[i],b)==0){
-                      t[i]=NULL;
-		    redirect(t,t[i+1],b);
-	          }
-		}*/
-             // path(t);
-              }
-    
-             else {
-             
-             
-          /* This is the parent process.  */
-          p->pid = pid;
-          if (shell_is_interactive)
-            {
-              if (!p->pgid)
-                p->pgid = pid;
-              setpgid (pid, p->pgid);
-            }
-                                           /* for the parent:      */
-         while (wait(&status) != pid)       /* wait for completion  */
-             ;
-            }
-           if(errno){
-      fprintf(stdout, "This shell only supports built-ins. Replace this to run programs as commands.\n");
-          }
-
-         }
+  
+    t = getToks(s); /* break the line into tokens */
+    fundex = lookup(t[0]); /* Is first token a shell literal */
+    if(fundex >= 0) cmd_table[fundex].fun(&t[1]);
+     
+    else {
        
-        fprintf(stdout, "%d%s: ", ++lineNum,getcwd(cwd,sizeof(cwd)));
-    }
+     if ((pid = fork()) < 0) {     /* fork a child process           */
+          printf("*** ERROR: forking child process failed\n");
+          exit(1);
+     }
 
-    return 0;
+     else if (pid == 0) {    /* for the child process:         */
+      process * new =create_process(temp);
+          add_process(new);
+             launch_process(new);
+      
+
+      int i;
+      char * command1=">", * command2= "<";
+      char *amp ="&";
+    for(i=0;i<MAXTOKS && t[i];i++){
+              if(strcmp(t[i],amp)== 0){
+                t[i]=NULL;
+            cmd_and(t[i],amp);
+   }
+            
+      
+	if (strcmp(t[i],command1)==0){
+	   t[i]=NULL;
+	    Redirecting_In_Out(t,t[i+1],command1);	
+          }
+        if(strcmp(t[i],command2)==0){
+           t[i]=NULL;
+            Redirecting_In_Out(t,t[i+1],command2);
+	    }
+	  }
+	  
+          path(t);
+
+
+        }
+        
+      else {    /* for the parent:      */
+	wait(&status);
+         /* wait for completion  */
+           
+    }
+      
+      fprintf(stdout, "This shell only supports built-ins. Replace this to run programs as commands.\n");
+     
+     }
+    fprintf(stdout, "%d%s:",++lineNum,getcwd(cwd,sizeof(cwd)));
+   }
+   
+     
+  return 0;
 }
